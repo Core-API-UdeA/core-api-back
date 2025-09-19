8fd9fd9941f1f2ed5d1f359fc913cb825b1a9e65
Alfonsin2020 <lalfonso.castano@udea.edu.co>
2025-09-18 23:56:59 -0500

    Agregar controladores para mostrar versiones de APIs, FileAssets, endpoints, organizaciones, miembros de organizaciones y planes con soporte de paginación y filtros.

---
+++ api/controllers/Endpoints/Listar-endpoints-apis.js
@@ -0,0 +1,86 @@
+smodule.exports = {
+  friendlyName: "Listar endpoints",
+
+  description: "Se listan endpoints de las apis registradas.",
+
+  inputs: {
+    pagination: {
+      description: 'paginacion.',
+      example: 'Objeto paginacion ',
+      type: 'ref',
+      required: true,
+    },
+
+    filter: {
+      description: 'Filtro.',
+      example: 'Objeto Filtro ',
+      type: 'ref',
+      required: true,
+    },
+  },
+
+  exits: {
+    success: {
+      description: "Finalización satisfactoria para enviar OK",
+      responseType: "okResponse",
+    },
+
+    errorGeneral: {
+      description: "Un error sin identificar generado en el try/catch.",
+      responseType: "nokResponse",
+    },
+  },
+  fn: async function ({ pagination, filter }, exits) {
+    sails.log.verbose('-----> Listar endpoints de las apis');
+    var flaverr = require('flaverr');
+    try {
+      let { page, rowsPerPage, sortBy, descending } = pagination;
+      const startRow = (page - 1) * rowsPerPage;
+      let filtroSortBy = [];
+      let limite = +pagination.limite;
+      let sort = {};
+      if (sortBy) {
+        sails.log.verbose('con sortBy', sortBy);
+        sort[sortBy] = descending === 'true' ? 'DESC' : 'ASC';
+        filtroSortBy = [sort];
+      } else {
+        filtroSortBy = [{ method: 'ASC' }];
+      }
+
+      let filtroWhere = {};
+
+      let queryCount = {
+        where: filtroWhere,
+      };
+
+      let queryTabla = {
+        where: filtroWhere,
+        skip: startRow,
+        limit: limite,
+        sort: filtroSortBy,
+      };
+
+      pagination.rowsNumber = await Endpoints.count(queryCount);
+
+      const api_Endpoints = await Endpoints.find(queryTabla);
+
+      return exits.success({
+        mensaje: 'Listar los endpoints de las apis fue correcto',
+        datos: {
+          pagination: pagination,
+          api_Endpoints: api_Endpoints
+        }
+      });
+
+
+    } catch (error) {
+      throw flaverr(
+        {
+          code: 'E_LISTA_APIS_ENDPOINTS',
+        },
+        error
+      );
+    }
+
+  },
+};
