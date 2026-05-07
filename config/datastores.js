a59e37ada026b75a6a1ba298fc5461ca56575dbe
JuanLema14 <andres.lema1@udea.edu.co>
2026-05-06 23:55:27 -0500

    feat: update database configuration to use environment variables and add example .env file

--- config/datastores.js
+++ config/datastores.js
@@ -1,12 +1,12 @@
 module.exports.datastores = {
   default: {
     adapter: 'sails-postgresql',
-    url: process.env.DATABASE_URL || 'postgresql://postgres.jhklyfyioaatabjygdkd:Z9E7xMd-ixHw8UH@aws-1-us-east-2.pooler.supabase.com:5432/postgres?sslmode=require',
+    url: process.env.DATABASE_URL,
     timezone: '+0'
   },
   CoreApiDB: {
     adapter: 'sails-postgresql',
-    url: process.env.DATABASE_URL || 'postgresql://postgres.jhklyfyioaatabjygdkd:Z9E7xMd-ixHw8UH@aws-1-us-east-2.pooler.supabase.com:5432/postgres?sslmode=require',
+    url: process.env.DATABASE_URL,
     timezone: '+0'
   },
 };
