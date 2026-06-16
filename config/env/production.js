f97812b691379385c5b15480a635756d04dba56a
juan.lema14 <juan.lema1408@gmail.com>
2026-06-15 22:38:12 -0500

    feat: add production environment configuration

---
+++ config/env/production.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+module.exports = {
+
+  models: {
+    migrate: 'safe',
+    cascadeOnDestroy: false
+  },
+
+  blueprints: {
+    shortcuts: false
+  },
+
+  security: {
+    cors: {
+      allRoutes: true,
+      allowOrigins: [
+        'https://core-api-zeta.vercel.app'
+      ],
+      allowCredentials: true
+    }
+  },
+
+  session: {
+    cookie: {
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  },
+
+  log: {
+    level: 'info'
+  },
+
+  http: {
+    trustProxy: true,
+    cache: 0
+  },
+
+  custom: {
+    baseUrl: process.env.BASE_URL,
+    frontendUrl: process.env.FRONTEND_URL,
+    gatewayEncryptionKey: process.env.GATEWAY_ENCRYPTION_KEY
+  }
+
+};
