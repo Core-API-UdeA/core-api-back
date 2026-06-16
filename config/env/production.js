6863e8f5d1799ee6c0745e9c4264286a97218b62
juan.lema14 <juan.lema1408@gmail.com>
2026-06-15 22:42:39 -0500

    style: format production.js for consistency and readability

--- config/env/production.js
+++ config/env/production.js
@@ -1,46 +1,46 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-
   models: {
-    migrate: 'safe',
-    cascadeOnDestroy: false
+    migrate: "safe",
+    cascadeOnDestroy: false,
   },
 
   blueprints: {
-    shortcuts: false
+    shortcuts: false,
   },
 
   security: {
     cors: {
       allRoutes: true,
-      allowOrigins: [
-        'https://core-api-zeta.vercel.app'
-      ],
-      allowCredentials: true
-    }
+      allowOrigins: ["https://core-api-zeta.vercel.app"],
+      allowCredentials: true,
+    },
   },
 
   session: {
     cookie: {
       secure: true,
-      maxAge: 24 * 60 * 60 * 1000
-    }
+      maxAge: 24 * 60 * 60 * 1000,
+    },
   },
 
   log: {
-    level: 'info'
+    level: "info",
   },
 
   http: {
     trustProxy: true,
-    cache: 0
+    cache: 0,
   },
 
   custom: {
     baseUrl: process.env.BASE_URL,
     frontendUrl: process.env.FRONTEND_URL,
-    gatewayEncryptionKey: process.env.GATEWAY_ENCRYPTION_KEY
-  }
+    gatewayEncryptionKey: process.env.GATEWAY_ENCRYPTION_KEY,
+  },
 
+  sockets: {
+    onlyAllowOrigins: ["https://core-api-zeta.vercel.app"],
+  },
 };
